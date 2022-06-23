d3335231fc907d88e2ec684c0598c7dd77b4696b
RafaelOliveiraElias <rafaelias567@gmail.com>
2022-06-23 11:14:58 -0300

    feat: herança

---
+++ exemploPOO/Models/Professor.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exemploPOO.Models
+{
+    public class Professor : Pessoa
+    {
+        public int Salario { get; set; }
+    }
+}
--- exemploPOO/Program.cs
+++ exemploPOO/Program.cs
@@ -1,12 +1,35 @@
 ﻿using exemploPOO.Models;
 
+Aluno p2 = new Aluno();
+
+p2.Nota = 8;
+
+p2.Nome = "BobAluno";
+p2.Idade = 15;
+
+p2.Apresentar();
+
+
+
+Professor p3 = new Professor();
+
+p3.Salario = 3000;
+p3.Nome = "BobProfessor";
+p3.Idade = 35;
+
+p3.Apresentar();
+
+
+
+System.Console.WriteLine("-----------ENCAPSULAMENTO:--------------");
+
 Retangulo r = new Retangulo();
 r.DefinirMedidas(30, 30);
 System.Console.WriteLine($"Área: {r.ObterArea()}");
 
 
 
-System.Console.WriteLine("-------------------------");
+System.Console.WriteLine("-----------ABSTRAÇÂO:--------------");
 Pessoa nova = new Pessoa();
 
 nova.Nome = "Bob";
